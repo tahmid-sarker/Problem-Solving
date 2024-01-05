781be1e963075bc7637902f582f9f1454b8792a2
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-06 03:25:37 +0600

    solve(beecrowd): `Area of a Circle`

---
+++ Beecrowd/Solutions/Area of a Circle.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double A, R, n = 3.14159;
+    cin >> R;
+    A = n * pow(R, 2);
+    cout << fixed << setprecision(4) << "A=" << A << endl;
+    return 0;
+}
