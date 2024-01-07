75cbe5ccce7a3c2ef8a6b05316d5cf75a3aa6ab7
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-07 17:01:15 +0600

    docs(beecrowd): solve `Salary` problem

---
+++ Beecrowd/Solutions/Salary.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int Number, Hour;
+    float Amount, Salary;
+    cin >> Number >> Hour >> Amount;
+    Salary = (Amount * Hour);
+    cout << "NUMBER = " << Number << endl;
+    cout << "SALARY = U$ " << fixed << setprecision(2) << Salary << endl;
+    return 0;
+}
