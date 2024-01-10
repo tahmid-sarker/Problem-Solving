e9244e9e4356a6ee4044790f317d54891e1acb35
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-10 06:05:00 +0000

    Add `Average 1.cpp`

---
+++ Beecrowd/Solutions/Average 1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    float A, B, Average;
+    cin >> A >> B;
+    Average = (A * 3.5 + B * 7.5) / (3.5 + 7.5);
+    cout << "MEDIA = " << fixed << setprecision(5) << Average << endl;
+    return 0;
+}
