493219aea72ac41e0f30923175115bc7a1d57c98
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-11 07:31:39 +0600

    solve(beecrowd): `Difference`

---
+++ Beecrowd/Solutions/Difference.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A, B, C, D, Difference;
+    cin >> A >> B >> C >> D;
+    Difference = (A * B - C * D);
+    cout << "DIFERENCA = " << Difference << endl;
+    return 0;
+}
