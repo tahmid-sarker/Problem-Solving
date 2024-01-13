ed34be78f4fbdc173e33288c805ab5fe413bfd5d
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-13 09:31:39 +0600

    solve(beecrowd): `Salary with Bonus`

---
+++ Beecrowd/Solutions/Salary with Bonus.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char Name[100];
+    cin >> Name;
+    double Fixed_Salary, Total_Sales, Total_Salary;
+    cin >> Fixed_Salary >> Total_Sales;
+    Total_Salary = Fixed_Salary + (Total_Sales * 15)/100;
+    cout << fixed;
+    cout << setprecision(2) << "TOTAL = R$ " << Total_Salary << endl;
+    return 0;
+}
