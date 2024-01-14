ce83bb3b918b01a5d5511dcf1df6feea01c3e161
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-14 10:31:39 +0600

    solve(beecrowd): `Simple Calculate`

---
+++ Beecrowd/Solutions/Simple Calculate.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    float x, y, result;
+    cin >> a >> c >> x >> b >> d >> y;
+    result = (c * x + d * y);
+    cout << "VALOR A PAGAR: R$ " << fixed << setprecision(2) << result << endl;
+    return 0;
+}
