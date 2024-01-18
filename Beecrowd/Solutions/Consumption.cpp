ce9fb65c5cada73d7cfab2639c3cc6d590c5bac6
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-18 14:31:39 +0600

    solve(beecrowd): `Consumption`

---
+++ Beecrowd/Solutions/Consumption.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    float X, Y, Result;
+    cin >> X >> Y;
+    Result = (float)X / Y;
+    cout << fixed << setprecision(3) << Result << " km/l" << endl;
+    return 0;
+}
