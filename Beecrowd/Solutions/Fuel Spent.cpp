aa1fbb80375730003fdf7a0dcbe9bfec9b9398ba
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-01-22 07:10:00 +0000

    Add `Fuel Spent.cpp`

---
+++ Beecrowd/Solutions/Fuel Spent.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int spent_time, average_speed;
+    double liters;
+    cin >> spent_time >> average_speed;
+    liters = (spent_time * average_speed) / 12.0;
+    cout << fixed;
+    cout << setprecision(3) << liters << endl;
+    return 0;
+}
