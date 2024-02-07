95a4ea552419b8f3622bd28ffa8b147d242d24ff
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-02-07 10:26:09 +0600

    solve(beecrowd): `Triangle Types`

---
+++ Beecrowd/Solutions/Triangle Types.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double A, B, C;
+    cin >> A >> B >> C;
+    if (A >= B + C || B >= A + C || C >= A + B) {
+        cout << "NAO FORMA TRIANGULO" << endl;
+    } else if (A * A == B * B + C * C || B * B == A * A + C * C || C * C == A * A + B * B) {
+        cout << "TRIANGULO RETANGULO" << endl;
+    } else if (A * A > B * B + C * C || B * B > A * A + C * C || C * C > A * A + B * B) {
+        cout << "TRIANGULO OBTUSANGULO" << endl;
+    } else if (A * A < B * B + C * C || B * B < A * A + C * C || C * C < A * A + B * B) {
+        cout << "TRIANGULO ACUTANGULO" << endl;
+    } else if (A == B && B == C) {
+        cout << "TRIANGULO EQUILATERO" << endl;
+    } else if (A == B && A != C || B == C && B != A || C == A && C != B) {
+        cout << "TRIANGULO ISOSCELES" << endl;
+    }
+    return 0;
+}
