89c7cc2d7653a85c27232e28139c32d58ef68063
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-02-07 07:27:10 +0600

    docs(beecrowd): solve `Prime Number` problem

---
+++ Beecrowd/Solutions/Prime Number.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, X, count;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> X;
+        count = 0;
+        for (int j = 2; j < X; j++) {
+            if (X % j == 0) {
+                count++;
+            }
+        }
+        if (count == 0) {
+            cout << X << " eh primo" << endl;
+        } else {
+            cout << X << " nao eh primo" << endl;
+        }
+    }
+    return 0;
+}
