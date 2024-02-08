cc87b58ba4e917240b6c8a8650cef53010ef70cd
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-02-08 09:17:10 +0600

    docs(beecrowd): solve `Blobs` problem

---
+++ Beecrowd/Solutions/Blobs.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, Day;
+    float X;
+    cin >> N;
+    while (N--) {
+        Day = 0;
+        cin >> X;
+        while (X > 1) {
+            X /= 2;
+            Day++;
+        }
+        cout << Day << " dias" << endl;
+    }
+    return 0;
+}
