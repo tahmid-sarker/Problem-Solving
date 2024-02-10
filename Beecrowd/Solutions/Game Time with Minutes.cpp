38232e69787b8fb2f2e4d2a1c5412fe71b3c00d1
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-02-10 02:20:00 +0000

    Add `Game Time with Minutes.cpp`

---
+++ Beecrowd/Solutions/Game Time with Minutes.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int initial_hour, initial_minute, final_hour, final_minute;
+    int duration;
+    cin >> initial_hour >> final_hour >> initial_minute >> final_minute;
+    duration = ((initial_minute * 60) + final_minute) - ((initial_hour * 60) + final_hour);
+    if (duration <= 0) {
+        duration += 24 * 60;
+    }
+    cout << "O JOGO DUROU " << duration / 60 << " HORA(S) E " << duration % 60 << " MINUTO(S)" << endl;
+    return 0;
+}
