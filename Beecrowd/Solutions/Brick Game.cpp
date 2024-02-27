936fa501111e40912b0ee9052b3e0dc3b2147f51
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-02-27 22:21:06 +0600

    docs(beecrowd): solve `Brick Game` problem

---
+++ Beecrowd/Solutions/Brick Game.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    for (int i = 1; i <= T; i++) {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int j = 0; j < N; j++) {
+            cin >> arr[j];
+        }
+        cout << "Case " << i << ": " << arr[N / 2] << endl;
+    }
+    return 0;
+}
