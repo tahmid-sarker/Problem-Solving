9235a15c360ea2d3191531bebe606d6b382d446e
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-02-29 07:14:58 +0600

    solve(beecrowd): `Sequence of Numbers and Sum`

---
+++ Beecrowd/Solutions/Sequence of Numbers and Sum.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    while (1) {
+        int M, N, temp, Sum = 0;
+        cin >> M >> N;
+        if (M <= 0 || N <= 0) {
+            break;
+        } else {
+            if (M > N) {
+                temp = M;
+                M = N;
+                N = temp;
+            }
+            for (int i = M; i <= N; i++) {
+                cout << i << " ";
+                Sum += i;
+            }
+            cout << "Sum=" << Sum << endl;
+        }
+    }
+    return 0;
+}
