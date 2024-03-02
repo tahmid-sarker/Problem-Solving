eac20c995971b29e263c3e6929c96a23d501ff0a
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-03-02 09:24:17 +0600

    solve(beecrowd): `Dividing X by Y`

---
+++ Beecrowd/Solutions/Dividing X by Y.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, X, Y;
+    float Division;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> X >> Y;
+        if (Y == 0) {
+            cout << "divisao impossivel" << endl;
+        } else {
+            Division = (float)X / Y;
+            cout << fixed << setprecision(1) << Division << endl;
+        }
+    }
+    return 0;
+}
