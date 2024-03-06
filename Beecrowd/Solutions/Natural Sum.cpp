a85777c2b31dbe27783c051f7d0cde4484b9c060
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-03-06 12:17:49 +0600

    docs(beecrowd): solve `Natural Sum` problem

---
+++ Beecrowd/Solutions/Natural Sum.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int A, B, Sum;
+    cin >> A >> B;
+    Sum = (A + B) * (B - A + 1) / 2;
+    cout << Sum << endl;
+    return 0;
+}
