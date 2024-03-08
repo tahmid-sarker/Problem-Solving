99f7edcf8ac02b512fd4fcc8cfe48d25a9024a95
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-03-08 14:24:17 +0600

    solve(beecrowd): `Growing Sequences`

---
+++ Beecrowd/Solutions/Growing Sequences.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    while (1) {
+        int X;
+        cin >> X;
+        if (X == 0) {
+            break;
+        } else {
+            for (int i = 1; i < X; i++) {
+                cout << i << " ";
+            }
+        }
+        cout << X << endl;
+    }
+    return 0;
+}
