1b9a3d60269f24ed35dafaaa7502cabcff8ab227
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-03-09 17:17:49 +0600

    docs(beecrowd): solve `Our Days Are Never Coming Back` problem

---
+++ Beecrowd/Solutions/Our Days Are Never Coming Back.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char ch[] = "LIFE IS NOT A PROBLEM TO BE SOLVED";
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cout << ch[i];
+    }
+    cout << endl;
+    return 0;
+}
