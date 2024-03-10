d617f32740571dd68ae099fd5307f68c1b489eb7
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-03-10 18:17:49 +0600

    docs(beecrowd): solve `Bill` problem

---
+++ Beecrowd/Solutions/Bill.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int C, N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> C;
+        if (C % 2 == 0) {
+            cout << "0" << endl;
+        } else {
+            cout << "1" << endl;
+        }
+    }
+    return 0;
+}
