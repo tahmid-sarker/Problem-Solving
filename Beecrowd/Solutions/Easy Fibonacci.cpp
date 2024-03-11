4c3e25865cb7ba8e414b3b84a3e8c36dfa945501
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-03-11 10:30:00 +0000

    Add `Easy Fibonacci.cpp`

---
+++ Beecrowd/Solutions/Easy Fibonacci.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int First = 0, Second = 1, Count = 0, Fibo, N;
+    cin >> N;
+    while (Count < N - 1) {
+        cout << First << " ";
+        Fibo = First + Second;
+        First = Second;
+        Second = Fibo;
+        Count++;
+    }
+    cout << First << endl;
+    return 0;
+}
