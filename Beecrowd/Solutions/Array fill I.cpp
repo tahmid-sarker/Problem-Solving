780cbb42d3a17c09f4ffe51196a94f03090ac794
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-03-19 02:38:06 +0600

    solve(beecrowd): `Array fill I`

---
+++ Beecrowd/Solutions/Array fill I.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 0; i <= 9; i++) {
+        cout << "N[" << i << "]" << " = " << n << endl;
+        n *= 2;
+    }
+    return 0;
+}
