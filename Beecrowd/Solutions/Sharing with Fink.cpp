f6ed3a44665227674d819b0945e95894bd212258
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-03-20 14:43:03 +0600

    docs(beecrowd): solve `Sharing with Fink` problem

---
+++ Beecrowd/Solutions/Sharing with Fink.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    while (cin >> N && N != 0) {
+        int sum = 0;
+        for (int i = 1; i <= N; i++) {
+            if (sum + i <= N) {
+                sum += i;
+            }
+        }
+        cout << sum << " " << N - sum << endl;
+    }
+    return 0;
+}
