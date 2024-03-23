de9fe7bec1aa2300ed298f17e327c1cbaa4d4736
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-03-23 11:35:00 +0000

    Add `Array Fill II.cpp`

---
+++ Beecrowd/Solutions/Array Fill II.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, count = 0;
+    cin >> n;
+    for (int i = 0; i < 1000; i++) {
+        cout << "N[" << i << "]" << " = " << count++ << endl;
+        if (count == n) {
+            count = 0;
+        }
+    }
+    return 0;
+}
