3b01a881a0b165c2dc1ccbe142002fa9ecf24159
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-04-04 17:38:06 +0600

    solve(beecrowd): `Hashmat the Brave Warrior`

---
+++ Beecrowd/Solutions/Hashmat the Brave Warrior.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int Hashmats_army, Opponents_army, Difference;
+    while (cin >> Hashmats_army >> Opponents_army) {
+        Difference = abs(Hashmats_army - Opponents_army);
+        cout << Difference << endl;
+    }
+    return 0;
+}
