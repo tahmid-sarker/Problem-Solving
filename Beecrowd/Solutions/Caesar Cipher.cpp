16093f40d0199888b61b8f2570dfc3420fd4c9ca
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-04-11 01:04:43 +0600

    solve(beecrowd): `Caesar Cipher`

---
+++ Beecrowd/Solutions/Caesar Cipher.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, X;
+    cin >> N;
+    char str[50], A;
+    while (N--) {
+        cin >> str >> X;
+        for (int i = 0; i < strlen(str); i++) {
+            A = str[i] - X;
+            if (A < 65) {
+                A += 26;
+            }
+            cout << A;
+        }
+        cout << endl;
+    }
+    return 0;
+}
