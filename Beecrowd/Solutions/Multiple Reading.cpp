c38bc301995e792537ba4a44bb53095ac6cc3cb9
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-04-13 03:04:43 +0600

    solve(beecrowd): `Multiple Reading`

---
+++ Beecrowd/Solutions/Multiple Reading.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string trace;
+    int P, size, count, cycle;
+    while (cin >> trace >> P) {
+        size = trace.size();
+        count = 0, cycle = 0;
+        for (int i = 0; i < size; i++) {
+            if (trace[i] == 'R') {
+                if (count == 0) {
+                    cycle++;
+                }
+                count++;
+                if (count == P) {
+                    count = 0;
+                }
+            } else {
+                cycle++;
+                count = 0;
+            }
+        }
+        cout << cycle << endl;
+    }
+    return 0;
+}
