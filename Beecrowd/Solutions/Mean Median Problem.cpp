eea044464f5e9406d79a93137f870713b7289c02
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-04-22 13:04:43 +0600

    solve(beecrowd): `Mean Median Problem`

---
+++ Beecrowd/Solutions/Mean Median Problem.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int A, B;
+    while (cin >> A >> B && A != 0 && B != 0) {
+        cout << 2 * A - B << endl;
+    }
+    return 0;
+}
