f49cff2317c15cfed929201ca31bb70cc0aedb55
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-05-02 23:04:43 +0600

    solve(beecrowd): `Making Kites`

---
+++ Beecrowd/Solutions/Making Kites.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, x, y, calculate;
+    cin >> N;
+    while (N--) {
+        cin >> x >> y;
+        if (x < 10 && y < 10) {
+            break;
+        }
+        calculate = (x * y) / 2;
+        cout << calculate << " cm2" << endl;
+    }
+    return 0;
+}
