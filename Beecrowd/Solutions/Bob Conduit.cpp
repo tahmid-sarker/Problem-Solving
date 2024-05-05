31b73a1ddb0f35f00946f46652751d8b2dc67522
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-05-05 10:55:00 +0000

    Add `Bob Conduit.cpp`

---
+++ Beecrowd/Solutions/Bob Conduit.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int R1, R2;
+        cin >> R1 >> R2;
+        cout << R1 + R2 << endl;
+    }
+    return 0;
+}
