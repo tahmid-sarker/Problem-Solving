064e4d84861f55f2bf6e5dbabad7a6d71ceb63aa
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-05-23 06:05:00 +0000

    Add `Jumping Frog.cpp`

---
+++ Beecrowd/Solutions/Jumping Frog.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int P, N;
+    cin >> P >> N;
+    int arr[N];
+    cin >> arr[0];
+    for (int i = 1; i < N; i++) {
+        cin >> arr[i];
+        if (abs(arr[i] - arr[i - 1]) > P) {
+            cout << "GAME OVER" << endl;
+            return 0;
+        }
+    }
+    cout << "YOU WIN" << endl;
+    return 0;
+}
