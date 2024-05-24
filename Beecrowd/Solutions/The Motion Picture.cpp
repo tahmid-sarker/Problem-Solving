7abafb53a3261e7f0c39c7b08a767deb650d23ac
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-05-24 07:05:00 +0000

    Add `The Motion Picture.cpp`

---
+++ Beecrowd/Solutions/The Motion Picture.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    float A, B;
+    cin >> A >> B;
+    float increase_price = B - A;
+    float increase_price_percent = (increase_price * 100.00) / A;
+    cout << fixed << setprecision(2) << increase_price_percent << "%" << endl;
+    return 0;
+}
