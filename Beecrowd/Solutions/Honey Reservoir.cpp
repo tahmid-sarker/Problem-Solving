53bbb76c46f886b21a6bd6261a72780a4a204c39
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-05-27 10:05:00 +0000

    Add `Honey Reservoir.cpp`

---
+++ Beecrowd/Solutions/Honey Reservoir.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double V, D, R, Area, Height;
+    while (cin >> V >> D) {
+        R = D / 2;
+        Area = 3.14 * pow(R, 2);
+        Height = V / Area;
+        cout << "ALTURA = " << fixed << setprecision(2) << Height << endl;
+        cout << "AREA = " << fixed << setprecision(2) << Area << endl;
+    }
+    return 0;
+}
