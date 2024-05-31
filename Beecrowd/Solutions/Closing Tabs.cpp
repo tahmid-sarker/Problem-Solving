26028948ed9577f4dd80e2077029bc4dae55f0fc
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-05-31 03:10:00 +0000

    Add `Closing Tabs.cpp`

---
+++ Beecrowd/Solutions/Closing Tabs.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, M;
+    string str;
+    cin >> N >> M;
+    while (M--) {
+        cin >> str;
+        if (str == "fechou") {
+            N++;
+        }
+        if (str == "clicou") {
+            N--;
+        }
+    }
+    cout << N << endl;
+    return 0;
+}
