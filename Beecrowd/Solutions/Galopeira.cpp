5ffb03d6e942823e648a2bc9b7b9ee909e916251
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-05-31 09:47:13 +0600

    solve(beecrowd): `Galopeira`

---
+++ Beecrowd/Solutions/Galopeira.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int C;
+    cin >> C;
+    cin.ignore();
+    while (C--) {
+        string str;
+        getline(cin, str);
+        int len = str.size();
+        double T = len * 0.01;
+        cout << fixed << setprecision(2) << T << endl;
+    }
+    return 0;
+}
