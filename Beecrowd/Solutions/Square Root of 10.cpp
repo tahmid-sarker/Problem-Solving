07e5c1b55bb09aace7507a12df68d5aac6d9f18d
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-06-04 08:10:00 +0000

    Add `Square Root of 10.cpp`

---
+++ Beecrowd/Solutions/Square Root of 10.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    double calculate = 0.0;
+    cin >> N;
+    while (N--) {
+        calculate += 6.00;
+        calculate = 1.00 / calculate;
+    }
+    calculate += 3.00;
+    cout << fixed << setprecision(10) << calculate << endl;
+    return 0;
+}
