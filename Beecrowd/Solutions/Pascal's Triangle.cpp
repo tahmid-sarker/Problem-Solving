2765fba21f1ff47670a4113a2c6f0174d1a19ff8
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-06-06 15:47:13 +0600

    solve(beecrowd): `Pascal's Triangle`

---
+++ Beecrowd/Solutions/Pascal's Triangle.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        long long int sum = pow(2, N) - 1;
+        cout << sum << endl;
+    }
+    return 0;
+}
