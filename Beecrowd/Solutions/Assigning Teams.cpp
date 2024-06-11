b975dde3dbf5882ad95a7bdd7a65f43b81a74f66
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-06-11 04:15:00 +0000

    Add `Assigning Teams.cpp`

---
+++ Beecrowd/Solutions/Assigning Teams.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A, B, C, D;
+    cin >> A >> B >> C >> D;
+    int difference = (A + D) - (B + C);
+    cout << abs(difference) << endl;
+    return 0;
+}
