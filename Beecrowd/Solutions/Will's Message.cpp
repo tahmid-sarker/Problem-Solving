e0de3e397c62b8b99b5c7f013157f6a7a34725a5
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-06-11 20:47:13 +0600

    solve(beecrowd): `Will's Message`

---
+++ Beecrowd/Solutions/Will's Message.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string messages;
+    while (cin >> messages) {
+        int number_of_commands, index;
+        cin >> number_of_commands;
+        for (int i = 0; i < number_of_commands; i++) {
+            cin >> index;
+            cout << messages[index - 1];
+        }
+        cout << endl;
+    }
+    return 0;
+}
