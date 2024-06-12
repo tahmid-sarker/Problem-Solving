ecd0b777d009147a14a23a4af69fa670866dfbf6
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-06-12 05:15:00 +0000

    Add `Brazil and Germany.cpp`

---
+++ Beecrowd/Solutions/Brazil and Germany.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    while (cin >> N && N != 0) {
+        double calculate = (double)N / 90.00;
+        int A, B;
+        B = floor(calculate * 1);
+        A = ceil(calculate * 7);
+        cout << "Brasil " << B << " x Alemanha " << A << endl;
+    }
+    return 0;
+}
