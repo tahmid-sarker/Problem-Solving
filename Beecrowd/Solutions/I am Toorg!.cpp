705fe9958596429b8d5d909f9e4712dccd6c4fc2
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-06-13 22:47:13 +0600

    solve(beecrowd): `I am Toorg!`

---
+++ Beecrowd/Solutions/I am Toorg!.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    string str;
+    while (N--) {
+        cin >> str;
+        cout << "I am Toorg!" << endl;
+    }
+    return 0;
+}
