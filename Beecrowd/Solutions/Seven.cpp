d3a95572e5fd4928cd2a53c897feface91971f7d
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-06-17 10:15:00 +0000

    Add `Seven.cpp`

---
+++ Beecrowd/Solutions/Seven.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    cout.sync_with_stdio(false);
+    int T;
+    cin >> T;
+    int digit[] = {1, 7, 9, 3};
+    while (T--) {
+        int N;
+        cin >> N;
+        cout << digit[N % 4] << "\n";
+    }
+    return 0;
+}
