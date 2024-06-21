919a4ec16cc20c10beafcd2da9ca3ccfeab237a0
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-06-21 06:13:51 +0600

    solve(beecrowd): `Date Input and Output`

---
+++ Beecrowd/Solutions/Date Input and Output.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int DD, MM, YY;
+    char slash;
+    cin >> DD >> slash >> MM >> slash >> YY;
+    cout << setw(2) << setfill('0') << MM << "/" << setw(2) << setfill('0') << DD << "/" << setw(2) << setfill('0') << YY << endl;
+    cout << setw(2) << setfill('0') << YY << "/" << setw(2) << setfill('0') << MM << "/" << setw(2) << setfill('0') << DD << endl;
+    cout << setw(2) << setfill('0') << DD << "-" << setw(2) << setfill('0') << MM << "-" << setw(2) << setfill('0') << YY << endl;
+    return 0;
+}
