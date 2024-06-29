09cd28db3c26b547d99dfae07fdcf449a1187a19
Md. Tahmid Sarker Mahi <tahmid@engineer.com>
2024-06-29 16:13:51 +0600

    solve(beecrowd): `Name at Form`

---
+++ Beecrowd/Solutions/Name at Form.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string L;
+    getline(cin, L);
+    if (L.length() <= 80) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
